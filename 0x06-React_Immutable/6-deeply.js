6b09f55dc5325890307157c1e9a75ffd66ab429c
lakelexy <alao8380@yahoo.com>
2023-08-16 14:14:44 +0000

    task6

--- 0x06-React_Immutable/6-deeply.js
+++ 0x06-React_Immutable/6-deeply.js
@@ -1,21 +1,17 @@
-import { Map } from 'immutable';
+import { Map, List } from "immutable";
 
-function mergeDeeplyRecursive(target, source) {
-    return target.withMutations(map => {
-        for (const key of source.keys()) {
-            if (map.has(key) && Map.isMap(map.get(key)) && Map.isMap(source.get(key))) {
-                map.set(key, mergeDeeplyRecursive(map.get(key), source.get(key)));
-            } else {
-                map.set(key, source.get(key));
-            }
-        }
-    });
+function mergeDeeplyElements(page1, page2) {
+  const map1 = Map(page1);
+  const map2 = Map(page2);
+
+  const mergedMap = map1.mergeDeepWith((prev, next) => {
+    if (List.isList(prev) && List.isList(next)) {
+      return prev.concat(next);
+    }
+    return next;
+  }, map2);
+
+  return List(mergedMap.values());
 }
 
-export function mergeDeeplyElements(page1, page2) {
-    const map1 = Map(page1);
-    const map2 = Map(page2);
-    
-    const mergedMap = mergeDeeplyRecursive(map1, map2);
-    return List(mergedMap.values());
-}
+export default mergeDeeplyElements;
