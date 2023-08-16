9d4699d1fe48f22d782d158055f945165fa17d74
lakelexy <alao8380@yahoo.com>
2023-08-16 13:27:43 +0000

    task1

--- 0x06-React_Immutable/1-map.js
+++ 0x06-React_Immutable/1-map.js
@@ -1,18 +1,7 @@
-const { Map } = require('immutable');
+import { Map } from "immutable";
 
-function getImmutableObject(obj) {
-    if (typeof obj !== 'object' || obj === null) {
-        throw new Error('Input must be a valid object');
-    }
-    return Map(obj);
+function getImmutableObject(object) {
+  return Map(object);
 }
 
-const inputObject = {
-    fear: true,
-    smell: -1033575916.9145899,
-    wall: false,
-    thing: -914767132
-};
-
-const immutableMap = getImmutableObject(inputObject);
-console.log(immutableMap);
+export default getImmutableObject.
