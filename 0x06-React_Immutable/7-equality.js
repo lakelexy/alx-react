269acf73520b45133b21e14c5c9130a12d5e77b3
lakelexy <alao8380@yahoo.com>
2023-08-17 11:53:05 +0000

    update

--- 0x06-React_Immutable/7-equality.js
+++ 0x06-React_Immutable/7-equality.js
@@ -1,5 +1,5 @@
-import { is, Map } from 'immutable';
+import { is } from 'immutable';
 
-export function areMapsEqual(map1, map2) {
-    return is(map1, map2)
-}
+const areMapsEqual = (map1, map2) => is(map1, map2);
+
+export default areMapsEqual;
